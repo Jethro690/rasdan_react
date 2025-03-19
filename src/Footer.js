d466052e1e7537905fce0dd01fc2d0ea94e5c5ed
Jethro690 <djethroe@gmail.com>
2025-03-19 22:32:21 +0200

    Centered info boxes with styled grid layout

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+            <p>Have a plant-tastic day 😊🌵</p>
+        </footer>
+    );
+}
+
+export default Footer;
