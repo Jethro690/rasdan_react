904cfda7b705b6e18a2dbe3b69cf64a4ee9237cf
Jethro690 <djethroe@gmail.com>
2025-03-23 16:27:27 +0200

    madeInfo a bit better

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import HowItWorks from './HowItWorks';
-import ContactDetails from './ContactDetails';
 
 function Info() {
     return (
         <section>
             <ul id="info-list">
-                <li>
-                    <HowItWorks />
-                </li>
-                <li>
-                    <ContactDetails />
-                </li>
+            <div className='info-boxlet'>
+            <h2>How It Works</h2>
+            <ul>
+                <li>Have a look at what's available on this page. See what you like?</li>
+                <li>Looking for a better picture of an item? Questions?</li>
+                <li>Message me on WhatsApp or Instagram. </li>
+                <li>Then we can set up an invoice and PUDO @ R 109.00 to any locker near you. 🥰</li>
+                <li>Get your pull!</li>
+                <li>🇿🇦 South Africa only 🇿🇦</li>
+                <ul>
+                <li>WhatsApp: <a href="https://wa.me/27643201946" target="_blank" rel="noopener noreferrer">0643201946</a></li>
+                <li>Instagram: <a href="https://www.instagram.com/rasdan_69" target="_blank" rel="noopener noreferrer">@rasdan_69</a></li>
+            </ul>
+            </ul>
+        </div>
             </ul>
 
         </section>
